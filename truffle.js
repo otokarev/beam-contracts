96ffa70bcc513ffd589acd91a76106a36e570514
Oleg Tokarev <otokarev@openprovider.ru>
2018-09-13 02:48:37 +0700

    [+] ecrecover tests added [+] npm deps added

--- truffle.js
+++ truffle.js
@@ -13,6 +13,13 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    migrations_directory: "./migrations",
+    networks: {
+        development: {
+            host: "10.1.30.50",
+            port: 8545,
+            gas: 210000000,
+            network_id: "*" // Match any network id
+        }
+    }
 };
